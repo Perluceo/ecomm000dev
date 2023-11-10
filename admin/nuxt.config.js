00391946649e8105b3cdfd7eb2b4401cdb7a3bc0
user <fullstackdevsmu@gmail.com>
2023-11-10 04:49:39 -0700

    nuxt config

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://ecomm000server-7471d1edeae9.herokuapp.com/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
